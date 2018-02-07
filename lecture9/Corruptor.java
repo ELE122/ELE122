faa1bf53da5fa492c27f4d06daf0e5ca5d7858ec
Andreas Urke <arur@hib.no>
2018-02-07 19:35:01 +0100

    lec9 WIP

--- lecture9/Corruptor.java
+++ lecture9/Corruptor.java
@@ -3,6 +3,7 @@ package lecture9;
 public class Corruptor implements Runnable {
 
 	String threadName;
+	static int counter = 0;
 	
 	public Corruptor(String threadName) {
 		this.threadName = threadName;
@@ -11,20 +12,28 @@ public class Corruptor implements Runnable {
 	@Override
 	public void run() {
 		while(true) {
-			System.out.println("Hello from " + threadName);
+			System.out.println("Hello from " + threadName + Thread.currentThread().getName());
+			System.out.println("Counter is now: " + counter);
 			try {
-				Thread.sleep(2000);
+				Thread.sleep(20);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
+			}
+			if(threadName.equals("Ole") || threadName.equals("Dole")) {
+				counter++;
+			}
+			else if(threadName.equals("Pluto") || threadName.equals("Doffen")) {
+				counter--;
 			}
 		}
 	}
 	
 	public static void main(String[] args) throws InterruptedException {
 		new Thread(new Corruptor("Ole")).start();
-		Thread.sleep(10);
 		new Thread(new Corruptor("Dole")).start();
 		new Thread(new Corruptor("Doffen")).start();
+		new Thread(new Corruptor("Pluto")).start();
+		System.out.println("Hello from " + Thread.currentThread().getName());
 	}
 
 
